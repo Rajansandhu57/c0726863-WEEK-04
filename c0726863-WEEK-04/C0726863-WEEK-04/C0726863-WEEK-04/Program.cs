b019df44f63e456c0e29e860b64a4dcf82b58f80
visualstudio <visualstudio@122-PC14>
2019-01-30 10:57:40 -0500

    hagsba

---
+++ c0726863-WEEK-04/C0726863-WEEK-04/C0726863-WEEK-04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C0726863_WEEK_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
